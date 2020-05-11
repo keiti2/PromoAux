4e8cf0da73b0e72f31cf054d6d1488aa624b8399
keiti2 <39093612+keiti2@users.noreply.github.com>
2020-05-10 22:16:50 -0300

    Lista produto preco

--- lib/ui/home/widget_ListaProduto.dart
+++ lib/ui/home/widget_ListaProduto.dart
@@ -3,40 +3,50 @@ import 'package:flutter/cupertino.dart';
 import './widget_DetalhesPromo.dart';
 import '../model/model.dart';
 
-class WidgetListPromocao extends StatelessWidget{
+class WidgetListPromocao extends StatelessWidget {
   final Promocao promocao;
   WidgetListPromocao(this.promocao);
 
- @override
+  @override
   Widget build(BuildContext context) {
     return new Container(
-      //padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(5.0),
       child: new Stack(
         children: <Widget>[
           new Container(
-          height: 100.0,
-          decoration: new BoxDecoration(color: Colors.black),
-          child: new ListTile(
-            leading: CircleAvatar(
-              radius: 35.0,
-              backgroundImage: AssetImage(promocao.image),
+            height: 100.0,
+            decoration: new BoxDecoration(color: Colors.black),
+            child: new ListTile(
+              leading: CircleAvatar(
+                radius: 35.0,
+                backgroundImage: AssetImage(promocao.image),
+              ),
+              title: Text(promocao.nome, style: TextStyle(color: Colors.white),textAlign: TextAlign.center),
+            
+              subtitle: new ListTile(
+                  title: Text(promocao.descricao, style: TextStyle(color: Colors.white,fontSize: 14)),
+                  subtitle: Text("De " + promocao.precoVenda.toString() + " Por  " + promocao.precoPromocao.toString(),style: TextStyle(color: Colors.white,fontSize: 15)),
+              ),
+
+
+
+               trailing:Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+
+              //teste merda
+              onTap: () {
+                print("hummmmm ????????");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => DetalhesPromo(promocao)),
+                );
+              },
             ),
-            title:
-                Text(promocao.nome, style: TextStyle(color: Colors.white)),
-            subtitle: Text(
-                promocao.descricao,
-                style: TextStyle(color: Colors.white)),
-            onTap: () {
-              print("hummmmm ????????");
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DetalhesPromo(promocao)),
-              );
-            },
           ),
-        ),
         ],
-    ),
+      ),
     );
   }
-  }
+}
+
+/////
