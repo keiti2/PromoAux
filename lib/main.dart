3761a13ee63c30c1ddf4c44b41d97a76f80b9e2f
keiti2 <39093612+keiti2@users.noreply.github.com>
2020-05-11 22:13:02 -0300

    quase um splash screen de enfeite

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:PromoPlus/ui/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:PromoPlus/ui/splashScreen.dart';
 
-/* void main()   {
-  runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Promo ++",
-    home: new HomePage(),
-  ));
-} */
 
-void main() {
+/* void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
@@ -20,4 +14,22 @@ void main() {
       home: new HomePage(),
     ));
   });
+}
+ */
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Promo ++',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Splash(),
+    );
+  }
 }
