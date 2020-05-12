c15d595573c2974e8c4eda994ab13f55263e45bb
keiti2 <39093612+keiti2@users.noreply.github.com>
2020-05-11 22:43:15 -0300

    revert commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:PromoPlus/ui/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:PromoPlus/ui/splashScreen.dart';
 
+/* void main()   {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Promo ++",
+    home: new HomePage(),
+  ));
+} */
 
-/* void main() {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
@@ -14,22 +20,4 @@ import 'package:PromoPlus/ui/splashScreen.dart';
       home: new HomePage(),
     ));
   });
-}
- */
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Promo ++',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Splash(),
-    );
-  }
 }
